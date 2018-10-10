86bef25ea12033bf286683f0efdd799a31a473da
“PDSDotNet” <pabloscuteri@gmail.com>
2018-10-09 21:31:13 -0300

    Agregado de seguridad, Con problemas al entrar al administrador

---
+++ core/AspNetCoreTodo1/Models/ManageUserViewModel.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using AspNetCoreTodo.Models;
+
+
+namespace AspNetCoreTodo.Models
+{
+    public class ManageUsersViewModel
+        {
+        public ApplicationUser[] Administrators { get; set; }
+        
+        public ApplicationUser[] Everyone { get; set;}
+    }
+}
