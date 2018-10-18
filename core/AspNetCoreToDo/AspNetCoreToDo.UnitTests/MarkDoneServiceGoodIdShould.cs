54a153a124b3fc6460f41b3f22a2a91e48378262
“PDSDotNet” <pabloscuteri@gmail.com>
2018-10-18 08:43:33 -0300

    AspNetCoreToDo. Se agrego un Test para verificar que el servicio MarkDone retorne TRUE cuando marca correctamente un ToDoItem. Hasta la pagina 105

--- core/AspNetCoreToDo/AspNetCoreToDo.UnitTests/MarkDoneServiceGoodIdShould.cs
+++ core/AspNetCoreToDo/AspNetCoreToDo.UnitTests/MarkDoneServiceGoodIdShould.cs
@@ -10,39 +10,43 @@ using Xunit;
 
 namespace AspNetCoreToDo.UnitTests
 {
-    public class ToDoItemServiceShould
+    public class MarkDoneServiceGoodIdShould
     {
         [Fact]
-        public async Task AddNewItemAsIncompleteWhitDueDate()
+        public async Task MarkDoneAnItemsWithGoodId()
         {
             //crea la base de datos en memoria.
             var options = new DbContextOptionsBuilder< ApplicationDbContext>()
-                .UseInMemoryDatabase( databaseName: "Test_AddNewItem").Options;
+                .UseInMemoryDatabase( databaseName: "Test_MarkDone").Options;
 
             //Crea el contexto necesario para realizar el test.
-            //En este caso ae crea el servicio ToDoItemService y el usuario ficticio
-            //y con esto se agrega un item a la base de datos.
+            //En este caso ae crea el servicio ToDoItemService() y el usuario ficticio
+            //y con esto se agrega un item a la base de datos. Luego se crea otro 
+            //servicio MarkDoneAsync() para marcar como completado al item. 
             using(var context = new ApplicationDbContext(options))
             {
                 var service = new ToDoItemService( context);
                 var fakeUser = new ApplicationUser{ Id= "fake-000", UserName= "fake@example.com"};
-                await service.AddItemAsync(new ToDoItem{ Title="Testing?"}, fakeUser);
+                var item = new ToDoItem{ Title="Testing?"};
+                await service.AddItemAsync( item, fakeUser);
+
+                //await service.MarkDoneAsync(item.Id, fakeUser);               
             }
 
             //Se crea otro contexto, para verificar que la base de datos
             //se halla completado correctamente. 
             using(var context = new ApplicationDbContext(options))
             {
+                var service = new ToDoItemService( context);
+                var fakeUser = new ApplicationUser{ Id= "fake-000", UserName= "fake@example.com"};
+                
                 var itemsInDataBase = await context.Items.CountAsync();
                 Assert.Equal(1, itemsInDataBase);
                 
                 var item = await context.Items.FirstAsync();
-                Assert.Equal("Testing?", item.Title);
-                //Assert.Equal(false, item.IsDone);
-                Assert.True( false == item.IsDone);
+                //Assert.True( item.IsDone);
+                Assert.True( await service.MarkDoneAsync(item.Id, fakeUser));
                 
-                var difference = DateTimeOffset.Now.AddDays(3)-item.DueAt;
-                Assert.True(difference < TimeSpan.FromSeconds(3));
             }
         }
     }
